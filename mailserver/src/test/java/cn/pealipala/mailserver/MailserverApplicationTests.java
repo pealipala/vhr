2727d10d723b072e770fda0240678aed3862d260
yechaoze <1029102110@qq.com>
2020-07-23 23:12:14 +0800

    搭建邮件服务

---
+++ mailserver/src/test/java/cn/pealipala/mailserver/MailserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pealipala.mailserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
